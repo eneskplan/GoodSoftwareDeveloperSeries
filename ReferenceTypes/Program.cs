6e0ff5fab393f2038766659d0169c0e5f4068551
Enes Kaplan <enesskaplann02@gmail.com>
2024-02-15 15:04:18 +0300

    Add project files.

---
+++ ReferenceTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ReferenceTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = 20;
+
+            sayi1 = sayi2;
+            sayi2 = 100;
+            Console.WriteLine("Sayı 1 : "+sayi1);
+        }
+    }
+}
