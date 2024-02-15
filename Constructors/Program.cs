88a77a0972ab4da7bcc4e65cb173256aab36d57d
Enes Kaplan <enesskaplann02@gmail.com>
2024-02-15 18:44:09 +0300

    Constructors

---
+++ Constructors/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer {Id=1,FirstName="Enes",LastName="Kaplan",City="İstanbul"};
+
+            Customer customer1 = new Customer(2,"Engin","Demiroğ","Ankara");
+            Console.WriteLine(customer1.FirstName);
+        }
+    }
+    class Customer
+    {
+
+        //default constructor
+        public Customer()
+        {
+            
+        }
+        public Customer(int id,string firstName,string lastName,string city)
+        {
+            Id= id;
+            FirstName= firstName;
+            LastName= lastName;
+            City= city;
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
